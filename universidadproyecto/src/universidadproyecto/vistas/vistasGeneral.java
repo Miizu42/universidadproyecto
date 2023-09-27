a48227126ef4c23b7d9131755ac4da6b90ab6a5d
paulinaroldan <roldanpaulina04@gmail.com>
2023-09-27 20:35:46 -0300

    Vista General
    Menu

--- universidadproyecto/src/universidadproyecto/vistas/vistasGeneral.java
+++ universidadproyecto/src/universidadproyecto/vistas/vistasGeneral.java
@@ -27,21 +27,90 @@ public class vistasGeneral extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenuAlumno = new javax.swing.JMenu();
+        jMItemFormularioAlumno = new javax.swing.JMenuItem();
+        jMenuMateria = new javax.swing.JMenu();
+        jMItemFormularioMateria = new javax.swing.JMenuItem();
+        jMenuAdministracion = new javax.swing.JMenu();
+        jMItemManejoInscripciones = new javax.swing.JMenuItem();
+        jMItemManipulacionNotas = new javax.swing.JMenuItem();
+        jMenuConsultas = new javax.swing.JMenu();
+        jMItemAlumnosPorMateria = new javax.swing.JMenuItem();
+        jMenuSalir = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 367, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 277, Short.MAX_VALUE)
+        );
+
+        jMenuAlumno.setText("Alumno");
+
+        jMItemFormularioAlumno.setText("Formulario de Alumno");
+        jMenuAlumno.add(jMItemFormularioAlumno);
+
+        jMenuBar1.add(jMenuAlumno);
+
+        jMenuMateria.setText("Materia");
+
+        jMItemFormularioMateria.setText("Formulario de Materia");
+        jMenuMateria.add(jMItemFormularioMateria);
+
+        jMenuBar1.add(jMenuMateria);
+
+        jMenuAdministracion.setText("Administración");
+
+        jMItemManejoInscripciones.setText("Manejo de Inscripciones");
+        jMenuAdministracion.add(jMItemManejoInscripciones);
+
+        jMItemManipulacionNotas.setText("Manipulacion de Notas");
+        jMItemManipulacionNotas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMItemManipulacionNotasActionPerformed(evt);
+            }
+        });
+        jMenuAdministracion.add(jMItemManipulacionNotas);
+
+        jMenuBar1.add(jMenuAdministracion);
+
+        jMenuConsultas.setText("Consultas");
+
+        jMItemAlumnosPorMateria.setText("Alumnos por Materia");
+        jMenuConsultas.add(jMItemAlumnosPorMateria);
+
+        jMenuBar1.add(jMenuConsultas);
+
+        jMenuSalir.setText("Salir");
+        jMenuBar1.add(jMenuSalir);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jDesktopPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jDesktopPane1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMItemManipulacionNotasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMItemManipulacionNotasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMItemManipulacionNotasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +148,17 @@ public class vistasGeneral extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenuItem jMItemAlumnosPorMateria;
+    private javax.swing.JMenuItem jMItemFormularioAlumno;
+    private javax.swing.JMenuItem jMItemFormularioMateria;
+    private javax.swing.JMenuItem jMItemManejoInscripciones;
+    private javax.swing.JMenuItem jMItemManipulacionNotas;
+    private javax.swing.JMenu jMenuAdministracion;
+    private javax.swing.JMenu jMenuAlumno;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenu jMenuConsultas;
+    private javax.swing.JMenu jMenuMateria;
+    private javax.swing.JMenu jMenuSalir;
     // End of variables declaration//GEN-END:variables
 }
